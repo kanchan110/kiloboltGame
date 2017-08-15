3a4cfb08c8d9a5fb57acd35e530fe37d7b7d0d40
kanchan95 <kanchan95@users.noreply.github.com>
2017-08-15 13:37:49 +0530

    Add files via upload

---
+++ src/kiloboltgame/Heilboy.java
@@ -0,0 +1,12 @@
+package kiloboltgame;
+
+public class Heilboy extends Enemy {
+
+	public Heilboy(int centerX, int centerY) {
+		// TODO Auto-generated constructor stub
+
+		setCenterX(centerX);
+		setCenterY(centerY);
+	}
+
+}
